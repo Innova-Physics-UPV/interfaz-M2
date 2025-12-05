dfd9f3ba555fe39ced80e2426ef04ae8fcc9955b
Camila_obn <camilaobonaga@gmail.com>
2025-12-05 18:06:41 +0100

    :(

---
+++ src/components/Basecard.svelte
@@ -0,0 +1,18 @@
+<div class="card text-center">
+  <slot> </slot>
+</div>
+<style>
+  .card {
+    background:#7D9AC6;
+    border-radius: 1px;
+    border: 5px solid #7D9AC6;
+    box-shadow: 0 4px 20px rgba(0,0,0,0.08);
+    padding: 1.5rem;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+  }
+</style>
---
+++ src/components/Grafica1.svelte
@@ -0,0 +1,32 @@
+<div class="card text-center">
+  <slot>  </slot>
+</div>
+<style>
+  .card {
+    /* --- 1. FONDO Y EFECTOS --- */
+    background: #ffffff;
+    /* PRUEBA ESTOS FONDOS: */
+    /*background: #1a1a1a; /*(Modo oscuro) */
+    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%); (Gradiente)
+    /*background: rgba(255, 255, 255, 0.5);/* */
+    border-radius: 1px;
+    border: 5px solid #C01313;
+    box-shadow: 0 4px 20px rgba(0,0,0,0.08);
+    /* PRUEBA: Cambia el color de la sombra a azul: rgba(0, 100, 255, 0.2) */
+
+    padding: 1.5rem;
+    height: 100%;
+
+    /* --- 2. DISPOSICIÓN (FLEXBOX) --- */
+    display: flex;
+
+    flex-direction: column;
+    /* PRUEBA: Cambia a 'row' para poner elementos uno al lado del otro */
+
+    justify-content: space-between;
+    /* PRUEBA: center, flex-start, flex-end, space-around */
+
+    /* align-items: center; */
+    /* Descomenta la linea de arriba para centrar el contenido horizontalmente */
+  }
+</style>
